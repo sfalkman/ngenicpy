507367a99f359604aa2ce594bd22c9e83521a235
Simon Falkman <simon.falkman@protegrity.com>
2019-09-04 23:00:25 +0200

    Changed directory name

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='ngenic-py',
+   version='1.0',
+   description='Python package for simple access to Ngenic Tune API',
+   license="Apache License 2.0",
+   long_description=long_description,
+   author='Simon Falkman',
+   author_email='sfalkman@gmail.com',
+   url="https://github.com/sfalkman/ngenic-py",
+   packages=['ngenic-py'],
+   install_requires=['requests']
+)

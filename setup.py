78a7dc7f06909cd7c116f428d30029476068a3bc
Simon Falkman <simon.falkman@protegrity.com>
2019-12-07 23:37:33 +0100

    Bump version to 0.1.2b1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='ngenicpy',
-   version='0.1.1',
+   version='0.1.2b1',
    description='Python package for simple access to Ngenic Tune API',
    license="MIT",
    long_description=long_description,
